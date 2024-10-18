bcdcda558aeb5e7ef6cb421846f6ae96ffe5ad83
Blank420 <mohitkalia09@gmail.com>
2024-10-19 02:14:17 +0530

    Developer baranch First

---
+++ SeleniumFrameworkDesign/src/test/java/Git/Branches_developer.java
@@ -0,0 +1,17 @@
+package Git;
+
+public class Branches_developer {
+	
+	@Test
+	public void brannch_1()
+	{
+		System.out.println("Inside Branch Developer part 1");
+		System.out.println("Inside Branch Developer part 2");
+
+		System.out.println("Inside Branch Developer part 3");
+
+		System.out.println("Inside Branch Developer part 4");
+
+	}
+
+}
